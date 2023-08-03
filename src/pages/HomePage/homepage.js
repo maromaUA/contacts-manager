f12e4759eab88a57d4e57ab8da95ba69eee01fa4
Roman Marchuk <maroma1991@gmail.com>
2023-08-03 18:39:11 +0300

    edit fix

--- src/pages/HomePage/homepage.js
+++ src/pages/HomePage/homepage.js
@@ -4,7 +4,6 @@ import {
   getContactsOperation,
   changeFavoriteOperation,
   deleteContactOperation,
-  updateContactOperation,
 } from '../../redux/contacts/operations';
 import { useEffect, useState } from 'react';
 import { getContacts } from '../../redux/contacts/selectors';
@@ -49,71 +48,17 @@ const Homepage = () => {
     dispatch(deleteContactOperation(id));
   };
 
-  async function editNameHandler(e, email, phone, favorite, id) {
-    console.log(e);
-    const body = {
-      name: e.currentTarget.textContent,
-      email,
-      phone,
-      favorite,
-    };
-    const result = await dispatch(updateContactOperation({ id, body }));
-    console.log('result:', result);
-    if (result.meta.requestStatus === 'rejected') {
-      alert('wrong name');
-    }
-  }
-
-  function editEmailHandler(e, name, phone, favorite, id) {
-    const body = {
-      email: e.currentTarget.textContent,
-      name,
-      phone,
-      favorite,
-    };
-    dispatch(updateContactOperation({ id, body }));
-  }
-
-  function editPhoneHandler(e, name, email, favorite, id) {
-    const body = {
-      phone: e.currentTarget.textContent,
-      name,
-      email,
-      favorite,
-    };
-    dispatch(updateContactOperation({ id, body }));
-    console.log(e);
-  }
-
   const result = contacts.map(({ _id, name, email, phone, favorite }) => (
     <li key={_id} className={css.contact}>
       <div className={css.liContent}>
         <p>
-          Name:{' '}
-          <span
-            contentEditable="true"
-            onInput={e => editNameHandler(e, email, phone, favorite, _id)}
-          >
-            {name}
-          </span>
+          Name: <span>{name}</span>
         </p>
         <p>
-          Email:{' '}
-          <span
-            contentEditable="true"
-            onInput={e => editEmailHandler(e, name, phone, favorite, _id)}
-          >
-            {email}
-          </span>
+          Email: <span>{email}</span>
         </p>
         <p>
-          Phone:{' '}
-          <span
-            contentEditable="true"
-            onInput={e => editPhoneHandler(e, name, email, favorite, _id)}
-          >
-            {phone}
-          </span>
+          Phone: <span>{phone}</span>
         </p>
       </div>
       <ul className={css.liActions}>
